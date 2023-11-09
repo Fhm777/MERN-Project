f224d8f5e747698cfe9a40ebcb3896dc7f931a70
jul-7 <124777800+jul-7@users.noreply.github.com>
2023-11-09 20:13:40 +0530

    Update layout.jsx

--- app/(auth)/layout.jsx
+++ app/(auth)/layout.jsx
@@ -1,9 +1,7 @@
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-        <body>
-            {children}
-        </body>
+      <body className="bg-slate-300">{children}</body>
     </html>
   );
 }
