6613558d23e3848a4ce8263ccab8ea13bb3e8e5a
Fhm777 <102980583+Fhm777@users.noreply.github.com>
2023-11-24 18:44:07 +0530

    navbar loading change

--- components/NavItems.jsx
+++ components/NavItems.jsx
@@ -38,6 +38,10 @@ const NavItems = () => {
             </div>
             <div className={`absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none ${showMenu ? "" : "hidden"}`} role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabIndex="-1">
                 {
+                  isLoading
+                  ?
+                  ""
+                  :
                   isSeller
                   ?
                   <div className="py-1" role="none">
