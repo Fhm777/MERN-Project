dbe3babfb21c537fece2c35c6b74572db5ecfc8d
Fhm777 <102980583+Fhm777@users.noreply.github.com>
2023-11-25 08:26:20 +0530

    fix order page

--- components/ProdTable.jsx
+++ components/ProdTable.jsx
@@ -78,7 +78,7 @@ function ProdTable({items, date, address}) {
                       :
                       ""
                     }
-                    <Link href="/"><button class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 focus:outline-none mt-2 w-full">Product</button></Link>
+                    <Link href={"/product/" + item.product_id}><button class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 focus:outline-none mt-2 w-full">Product</button></Link>
                   </div>
                 </div>
               </td>
